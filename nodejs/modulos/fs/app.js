44d369959b0e115b9abb8d3a304cb0e4e5fc49eb
Bernardo Casagrande <bernardocasagrande0@gmail.com>
2025-09-03 09:09:46 -0300

    versão atualizada

---
+++ nodejs/modulos/fs/app.js
@@ -0,0 +1,17 @@
+//importando o modulo fs (file system)
+const fs = require ('fs');
+
+//lendo um arquivo de forma assincrona
+fs.readFile('exempl.txt','utf8', (erro, dados) => {
+    //tratando erro
+    if (erro) {
+        //adicionando o erro no console
+    console.error ('Erro ao ler o arquivo:', erro);
+    //retornando para nao executar o console.log abaixo
+    return;
+}
+//se nao houver erro, exibindo conteudo do arquivo
+console.log ('Conteúdo do arquivo:', dados);
+});
+//imprimindo uma mensagem antes da leitura do arquivo
+console.log ('Esta mensagem aparece primeiro.');
